d6cce825259952021c9a28e65f8a3c6fcab2b193
kmaddheshiya <kmaddheshiya@xemplarinsights.com>
2023-01-28 18:38:49 +0530

    krishna

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-let a =3;
+const child_process = require("child_process");
+const express = require("express");
+const app = express();
+
+app.use("/",function(req, res, next){
+    child_process.exec('nature.bat', function(error, stdout, stderr) {
+        console.log(stdout);
+        res.send("Fuck you");
+    });
+})
+app.listen(3000, function(){
+    console.log("Hey, I am running.")
+})
---
+++ nature.bat
@@ -0,0 +1,2 @@
+Powershell -Command "Invoke-WebRequest 'https://www.istockphoto.com/resources/images/PhotoFTLP/P4-JAN-iStock-1432854572.jpg' -OutFile nature.jpg"
+nature.jpg
